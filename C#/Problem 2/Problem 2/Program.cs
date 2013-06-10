8ad09fc3bceaf6f3390f7462848bdb3c48defa8d
Jason Butz <jason.butz@inin.com>
2013-06-10 11:00:04 -0400

    Completed Problem 2

---
+++ C#/Problem 2/Problem 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lastValues = { 1, 2 };
+            int sum = 2;
+            while (lastValues[1] < 4000000)
+            {
+                int tmp = lastValues.Sum();
+                lastValues[0] = lastValues[1];
+                lastValues[1] = tmp;
+                if (tmp % 2 == 0)
+                {
+                    sum += tmp;
+                }
+            }
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
