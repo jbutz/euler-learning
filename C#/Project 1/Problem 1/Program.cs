ffd84828e501998fe5a9d9930959d2d15c47f688
Jason Butz <jason.butz@inin.com>
2013-06-10 10:29:45 -0400

    Completed Problem 1

---
+++ C#/Project 1/Problem 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            for (var i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine("Sum of all multiples of 3 or 5 below 1000: " + Convert.ToString(sum));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
