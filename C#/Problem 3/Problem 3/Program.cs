9aff39d75dc6046924e610958f3411d1619622f1
Jason Butz <jason.butz@inin.com>
2013-06-12 10:40:34 -0400

    Completed problem 3

---
+++ C#/Problem 3/Problem 3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong number = 600851475143;
+            ulong factor = 0;
+            for(ulong i = 1; i < number; i++)
+            {
+                if (i <= 0)
+                    break;
+                if (number % i == 0)
+                {
+                    Console.WriteLine("F: " + i);
+                    // It is a factor, is it prime?
+                    if (isPrime(i))
+                    {
+                        // It is prime
+                        if (i >= factor)
+                        {
+                            Console.WriteLine("P: " + i);
+                            factor = i;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Solution: " + factor);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+
+        static bool isPrime(ulong number)
+        {
+
+            for (ulong i = 2; i*i <= number; i++)
+            {
+                if (number % i == 0 && i != number)
+                    return false;
+            }
+            
+            return true;
+        }
+    }
+}
